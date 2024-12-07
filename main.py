cdb875726d15b380b3f329d787f5e06465bda95a
farukilic <farukkilicaga@gmail.com>
2024-12-07 18:58:06 +0300

    1.2
    with machine learning

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.naive_bayes import MultinomialNB
 
 ############################################################################
 """ Bu kısmı satırın en başındaki(virgülden önceki) kısmı alabilmek için yazdım.
@@ -22,6 +26,37 @@ df.to_csv("temizdataset.csv", index= False)
 
 """
 ########################################################################
-# Burada iki dataseti tek bir datasette topladım.
-df = pd.read_csv("labeled_data.csv")
+#Dosyayı okuyup ; değeri görünce sütun atlıyor
+df = pd.read_csv("dataset_words.csv", sep=";")
+
+#
+vektor = TfidfVectorizer(max_features=5000, ngram_range=(1, 2))
+X = vektor.fit_transform(df['text']).toarray()
+y = df['label']
+
+
+# Veriyi bölüyoruz bir kısmı ile makineyi eğitiyor bir kısmı ile test ediyoruz.
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=48)
+
+
+model = MultinomialNB()
+model.fit(X_train, y_train)
+
+
+# Test et ve sonuçları yazdır
+y_pred = model.predict(X_test)
+print("Doğruluk Oranı:", accuracy_score(y_test, y_pred))
+print(classification_report(y_test, y_pred))
+
+
+def check_message(message):
+    vectorized = vektor.transform([message]).toarray()
+    prediction = model.predict(vectorized)
+    if prediction[0] == 1:
+        return "Küfür içeriyor!"
+    else:
+        return "Temiz."
+
+
+print(check_message(input("Bir kelime giriniz:")))
 
